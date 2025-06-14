774a4d66b5a0f5db9bd5c4e0eda4a5b13135e96d
Malik Anas <malikanassultan@gmail.com>
2025-06-14 17:23:11 +0500

    second code commit

--- tests/Allmainaction.spec.ts
+++ tests/Allmainaction.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Practice all concept of playwright', () => {
     test('Try to upload file', async ({ page }) => {
-        
-    });
-});
+        await page.goto('https://demoqa.com/automation-practice-form')
+       // await page.getByTestId('[id="uploadPicture"]').click()
+    })
+})
