ff06a188454ebd57e74cf451c3337d541ef53efb
Malik Anas <malikanassultan@gmail.com>
2025-06-14 17:58:22 +0500

    Ci setup hope so done trigger two repo

--- tests/Allmainaction.spec.ts
+++ tests/Allmainaction.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test.describe('Practice all concept of playwright', () => {
     test('Try to upload file', async ({ page }) => {
         await page.goto('https://demoqa.com/automation-practice-form')
-       // await page.getByTestId('[id="uploadPicture"]').click()
+        await page.getByPlaceholder('First Name').fill('test')
     })
 })
