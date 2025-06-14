ab6be519365d8531c02eb2a98720ea43ba72a399
Malik Anas <malikanassultan@gmail.com>
2025-06-14 12:29:22 +0500

    Playwright setup done first code commit

---
+++ tests/Allmainaction.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Practice all concept of playwright', () => {
+    test('Try to upload file', async ({ page }) => {
+        
+    });
+});
